85ffebf0dec5c4e8be3d190efb00bd08a8b3c71a
cmfeltgood <cmfeltgood@gmail.com>
2024-04-14 11:27:47 -0400

    Add js functions

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
-alert("heck")
+//alert("heck");
 
 
+let mode = "none";
+let grid = [["none","none","none","none"],
+["none","none","none","none"],
+["none","none","none","none"],
+["none","none","none","none"],
+["none","none","none","none"]];
+
+
+
+function paintGrid(row, col) {
+  grid[row][col] = mode;
+}
+
+function setMode(newMode){
+  mode = newMode;
+}
+
+function getTemplateAreaString(){
+  str = "";
+  for (let i = 0; i < length(grid); i++){
+    str += "'";
+    for (let j = 0; j < length(grid[i]) ; j++){
+      str+= grid[i][j] + " ";
+    }
+    str += "'\n";
+  }
+}
+
+
+
+alert(getTemplateAreaString())
